60757825d54f237a7aad5929b84df69416476641
josemarionj1103 <josemarionj1103@gmail.com>
2026-02-14 10:11:10 -0400

    Update recursividad1.cpp

--- recursividad1.cpp
+++ recursividad1.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 using namespace std;
 
-long factorial(int n){
-    if(n == 0 || n == 1){   
+long factorialRecursivo(int n){
+    if(n == 0 || n == 1){
         return 1;
     }
     else{
-        return n * factorial(n - 1);  
+        return n * factorialRecursivo(n - 1);
     }
 }
+
+long factorialIterativo(int n){
+    long resultado = 1;
+    for(int i = 1; i <= n; i++){
+        resultado *= i;
+    }
+    return resultado;
+}
+
 int main(){
     
-    int n;
-    
-    cout<<"ingresar un numero: "; cin >> n;
-    
-    if(n<0){
-        cout<<"El numero debe ser mayor a 0"<<endl;
-        
+    int n, opcion;
+
+    cout << "Ingresar un numero: ";
+    cin >> n;
+
+    if(n < 0){
+        cout << "El numero debe ser mayor o igual a 0" << endl;
+        return 0;
     }
-    cout<<"El factorial de " <<n<< " es: "<<factorial(n)<< endl;
-    
-    
+
+    cout << "\nSeleccione el metodo:\n";
+    cout << "1. Metodo Iterativo\n";
+    cout << "2. Metodo Recursivo\n";
+    cout << "Opcion: ";
+    cin >> opcion;
+
+    if(opcion == 1){
+        cout << "\nFactorial (Iterativo) de " << n 
+             << " es: " << factorialIterativo(n) << endl;
+    }
+    else if(opcion == 2){
+        cout << "\nFactorial (Recursivo) de " << n 
+             << " es: " << factorialRecursivo(n) << endl;
+    }
+    else{
+        cout << "Opcion invalida." << endl;
+    }
+
     return 0;
 }
